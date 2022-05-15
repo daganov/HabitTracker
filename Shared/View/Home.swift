9b006a1fbf5750f13b37f980321f34d58b3992bc
Denis Aganov <den.aganov@yandex.ru>
2022-05-15 22:37:00 +0300

    List all habits

--- Shared/View/Home.swift
+++ Shared/View/Home.swift
@@ -29,10 +29,15 @@ struct Home: View {
                             .foregroundColor(.white)
                     }
                 }
+                .padding(.bottom, 10)
             
             // Установка кнопки добавления посередине, если привычек ещё нет
             ScrollView(habits.isEmpty ? .init() : .vertical, showsIndicators: false) {
                 VStack(spacing: 15) {
+                    ForEach(habits) { habit in
+                        HabitCardView(habit: habit)
+                    }
+                    
                     // MARK: Add Habit Button
                     Button {
                         habitModel.addNewHabit.toggle()
@@ -60,6 +65,85 @@ struct Home: View {
             AddNewHabit()
                 .environmentObject(habitModel)
         }
+    }
+    
+    // MARK: Habit Card View
+    @ViewBuilder
+    func HabitCardView(habit: Habit) -> some View {
+        VStack(spacing: 6) {
+            HStack {
+                Text(habit.title ?? "")
+                    .font(.callout)
+                    .fontWeight(.semibold)
+                    .lineLimit(1)
+                
+                Image(systemName: "bell.badge.fill")
+                    .font(.callout)
+                    .foregroundColor(Color(habit.color ?? "Card-1"))
+                    .scaleEffect(0.9)
+                    .opacity(habit.isRemainderOn ? 1 : 0)
+                
+                Spacer ()
+                
+                let count = (habit.weekDays?.count ?? 0)
+                Text(count == 7 ? "Ежедневно" : "\(count) в неделю")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+            }
+            .padding(.horizontal, 10)
+            
+            // MARK: Displaying Current Week and Marking Active Dates of Habit
+            let calendar: Calendar = {
+                var cal = Calendar.current
+                cal.locale = Locale(identifier: "ru")
+                return cal
+            }()
+            let currentWeek = calendar.dateInterval(of: .weekOfMonth, for: Date())
+            let symbols = calendar.shortWeekdaySymbols
+            let startDate = currentWeek?.start ?? Date()
+            let activeWeekDays = habit.weekDays ?? []
+            let activePlot = symbols.indices.compactMap { index -> (String, Date) in
+                let currentDate = calendar.date(byAdding: .day, value: index, to: startDate)
+                return (symbols[index], currentDate!)
+            }
+            
+            HStack(spacing: 0) {
+                ForEach(activePlot.indices, id: \.self) { index in
+                    let item = activePlot[index]
+                    
+                    VStack(spacing: 6) {
+                        Text(item.0)
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                        
+                        let status = activeWeekDays.contains { $0 == item.0 }
+                        
+                        Text(getDate(date: item.1))
+                            .font(.system(size: 14))
+                            .fontWeight(.semibold)
+                            .padding(8)
+                            .background {
+                                Circle()
+                                    .fill(Color(habit.color ?? "Card-1"))
+                                    .opacity(status ? 1 : 0)
+                            }
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+            }
+            .padding(.top, 15)
+        }
+        .padding(.vertical)
+        .padding(.horizontal, 6)
+        .fieldBackground()
+    }
+    
+    // MARK: Formatting Date
+    func getDate(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd"
+        
+        return formatter.string(from: date)
     }
 }
 
